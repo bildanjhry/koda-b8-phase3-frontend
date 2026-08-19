958c7984d5539bd33f5b0de5743f68b30d9f3c6e
Bildan Jauhary <bildanjauhary@gmail.com>
2026-08-19 14:47:15 +0700

    feat: do some refinement at footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,33 +1,41 @@
 import { Link } from "react-router";
 
 export default function Footer(){
+
+    const footerList = [
+        {
+            path:"",
+            name:"PRIVACY POLICY"
+        },
+        {
+            path:"",
+            name:"TERMS OF SERVICES"
+        },
+        {
+            path:"",
+            name:"API DOCUMENTATION"
+        },
+        {
+            path:"",
+            name:"SUPPORT"
+        },
+    ]
+
     return(
-        <div className="h-30.25 bg-(--base) px-10 flex items-center justify-between">
+        <div className="h-30.25 bg-(--base) font-semibold text-(--mute) 
+				px-10 w-full flex items-center justify-between">
             <section>
-                <p>© 2024 ShortLink. The Digital Architect.</p>
+                <p>© 2026 SHORTLINK. THE DIGITAL ARCHITECT.</p>
             </section>
             <section>
-                <ul className="flex gap-3">
-                    <li>
-                        <Link>
-                            Privacy Policy
-                        </Link>
-                    </li>
-                    <li>
-                        <Link>
-                            Privacy Policy
-                        </Link>
-                    </li>
-                    <li>
-                        <Link>
-                            Privacy Policy
-                        </Link>
-                    </li>
-                    <li>
-                        <Link>
-                            Privacy Policy
-                        </Link>
-                    </li>
+                <ul className="flex gap-15">
+                    { footerList.map((item, index) => (
+                        <li key={index}>
+                            <Link to={item.path}>
+                                {item.name}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </section>
         </div>
